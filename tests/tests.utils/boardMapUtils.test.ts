1be915f312dd7df42d1a9c384a5f1ab6b7049f00
Minimal-Alexi <alexpop@metropolia.fi>
2026-04-30 11:49:13 +0300

    Created unit tests for createBoardMap

--- tests/tests.utils/boardMapUtils.test.ts
+++ tests/tests.utils/boardMapUtils.test.ts
@@ -29,11 +29,62 @@ describe("Test createBoardMap function", () => {
         ];
         const boardMap2 = createBoardMap(fen2);
         expect(boardMap2).toEqual(expectedBoardMap2);
+
+        const fen3 = "8/8/8/8/8/8/8/1k6"
+        const expectedBoardMap3 = [ 
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', 'k', ' ', ' ', ' ', ' ', ' ', ' '],
+        ]
+        const boardMap3 = createBoardMap(fen3)
+        expect(boardMap3).toEqual(expectedBoardMap3)
+
+        const fen4 = "3k4/8/8/8/8/8/8/8"
+        const expectedBoardMap4 = [
+            [' ', ' ', ' ', 'k', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ]
+        const boardMap4 = createBoardMap(fen4)
+        expect(boardMap4).toEqual(expectedBoardMap4)
+
+        const fen5 = "8/8/8/8/8/8/8/7r"
+        const expectedBoardMap5 = [
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'r'],
+        ]
+        const boardMap5 = createBoardMap(fen5)
+        expect(boardMap5).toEqual(expectedBoardMap5)
     });
+    
     it("Should return null for an invalid FEN string", () => {
         const fen = "invalid-fen-string";
         const boardMap = createBoardMap(fen);
         expect(boardMap).toBeNull();
+
+        const fen2 = "8/8/8/8/8/8/8";
+        const boardMap2 = createBoardMap(fen2);
+        expect(boardMap2).toBeNull();
+
+        const fen3 = "8/8/8/8/8/8/8q";
+        const boardMap3 = createBoardMap(fen3);
+        expect(boardMap3).toBeNull();
     }
     )
 });
@@ -53,6 +104,21 @@ describe ("Test createFenString function", () => {
         const expectedFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
         const fen = createFenString(boardMap);
         expect(fen).toEqual(expectedFen);
+
+        const boardMap2 = [
+            ['r', ' ', 'b', 'q', 'k', 'b', 'n', 'r'],
+            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            [' ', ' ', 'n', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+        ];
+        const expectedFen2 = "r1bqkbnr/pppppppp/2n5/8/8/8/PPPPPPPP/RNBQKBNR";
+        const fen2 = createFenString(boardMap2)
+        expect(fen2).toEqual(expectedFen2)
+
     });
     it("Should return null for an invalid board map", () => {
         const boardMap = [
@@ -69,3 +135,9 @@ describe ("Test createFenString function", () => {
         expect(fen).toBeNull();
     });
 });
+
+describe("Test createFenString and createBoardMap compitability", () => {
+    it("createFenString and createBoardMap should be compatible with eachother.", () => {
+
+    })
+})
